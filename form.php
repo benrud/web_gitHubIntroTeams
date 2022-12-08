a3acff68ef0fcaaea1616d1eb2e94310edbdb839
Todd Benrud <benrud@users.noreply.github.com>
2022-12-08 18:40:33 +0000

    initial form

---
+++ form.php
@@ -0,0 +1,17 @@
+<form>
+  <label for="first-name">First Name:</label><br>
+  <input type="text" id="first-name" name="first-name"><br>
+  <label for="last-name">Last Name:</label><br>
+  <input type="text" id="last-name" name="last-name"><br>
+  <label for="email">Email:</label><br>
+  <input type="email" id="email" name="email"><br>
+  <label for="phone">Phone:</label><br>
+  <input type="tel" id="phone" name="phone"><br>
+  <label for="relationship">Relationship:</label><br>
+  <select id="relationship" name="relationship">
+    <option value="friend">Friend</option>
+    <option value="family">Family</option>
+    <option value="coworker">Coworker</option>
+  </select><br><br>
+  <input type="submit" value="Submit">
+</form> 
